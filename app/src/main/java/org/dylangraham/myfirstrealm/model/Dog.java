af3af8fe2740ac6f311db3c2fa1dcd245e5272dd
Dylan Graham <dylan.graham@gmail.com>
2017-01-24 17:53:33 +1100

    immutable once constructed

--- app/src/main/java/org/dylangraham/myfirstrealm/model/Dog.java
+++ app/src/main/java/org/dylangraham/myfirstrealm/model/Dog.java
@@ -6,4 +6,17 @@ package org.dylangraham.myfirstrealm.model;
 public class Dog extends RealmObject {
     private String name;
     private int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
