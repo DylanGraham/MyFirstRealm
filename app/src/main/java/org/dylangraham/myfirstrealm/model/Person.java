b1820c4e84991fdadfec17bbb502c7e8b8279b0d
Dylan Graham <dylan.graham@gmail.com>
2017-01-24 17:55:33 +1100

    immutable once constructed

---
+++ app/src/main/java/org/dylangraham/myfirstrealm/model/Person.java
@@ -0,0 +1,31 @@
+package org.dylangraham.myfirstrealm.model;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+
+public class Person extends RealmObject {
+    @PrimaryKey
+    private long id;
+    private String name;
+    private RealmList<Dog> dogs;
+
+    public Person(long id, String name, RealmList<Dog> dogs) {
+        this.id = id;
+        this.name = name;
+        this.dogs = dogs;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public RealmList<Dog> getDogs() {
+        return dogs;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
